94e29eea2427aa66c528abd460c8ad5261a5f17e
gatholocool <49301741+gatholocool@users.noreply.github.com>
2021-01-18 14:46:14 +0700

    Finito

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    
+    class Program
+    {
+        public static void Main(string[] args){
+            Console.WriteLine("Amankan, siap");
+        }
+    
+    }
+}
